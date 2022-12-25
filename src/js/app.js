7eba8c758cdd2b4ba6dd017ffde3500cff8e7327
Dimitar Malinov <mitachftw@gmail.com>
2022-12-25 15:09:29 +0200

    fix: moved the code in the app.js file

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,8 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const elements = document.querySelectorAll('.hot');
+  elements.forEach(el => {
+    el.textContent = el.textContent + '🔥';
+  });
 });
